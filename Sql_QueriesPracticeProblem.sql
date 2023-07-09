dd8d56c8fc6044138ab86f1cf242aa31cf12e68b
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-07-09 18:06:02 +0530

    [somu]Add:Updating the Customer Details Using Name.

--- Sql_QueriesPracticeProblem.sql
+++ Sql_QueriesPracticeProblem.sql
@@ -15,3 +15,8 @@ Update Customer set Country='India',Salary=30000,Pincode='562322';
 Update Customer set Country='England',Salary=29000,Pincode='234234' where Customer_Id=2
 Update Customer set Country='Russia',Salary=28000,Pincode='634445' where Customer_Id=3
 Update Customer set Country='Germany',Salary=29000,Pincode='345444' where Customer_Id=4
+--UC-5
+Update Customer set Phone='9899880077',Address='Banglore',Country='India' where Name='Somashekar'
+Update Customer set Phone='8787666876',Address='Chennai',Country='India' where Name='Anil'
+Update Customer set Phone='8899007766',Address='Delhi',Country='India' where Name='Manju'
+Update Customer set Phone='9900876655',Address='Mumbai',Country='India' where Name='Ganesh'
