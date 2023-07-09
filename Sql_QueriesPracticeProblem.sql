0c734b49e632316c92baf83eed19414b93c447f2
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-07-09 17:23:14 +0530

    [somu]Add:Creating Customer Table in the Database.

--- Sql_QueriesPracticeProblem.sql
+++ Sql_QueriesPracticeProblem.sql
@@ -1,2 +1,5 @@
 --UC-1
 Create Database CompanyData
+--UC-2
+Create table Customer (Customer_Id int primary key identity, Name varchar(20), Phone varchar(10), Address varchar(100))
+Select * from Customer
